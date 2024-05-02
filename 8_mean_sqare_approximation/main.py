5bb15dc4948e483a820dd5ec2f9a3f01d749e0d3
CALLmeDOMIN <dominiksieron13@gmail.com>
2024-05-02 18:21:44 +0200

    Done: lab8 done

--- 8_mean_sqare_approximation/main.py
+++ 8_mean_sqare_approximation/main.py
@@ -1,14 +1,6 @@
-from numeric_methods import gauss_legendre_formula, lu_decomposition, print_matrix, horner_interpolation, mse
-from numpy import sin as np_sin, pi as np_pi, exp as np_exp, linspace as np_linspace, array as np_array
+from numeric_methods import gauss_legendre_formula, lu_decomposition, horner_interpolation
+from numpy import sin as np_sin, exp as np_exp, linspace as np_linspace, array as np_array, abs as np_abs
 import matplotlib.pyplot as plt
-
-base = [1, 1, 1, 1, 1]
-start = 0
-end = np_pi / 2
-
-
-def f_sin(x):
-    return np_sin(x)
 
 
 def f_task(x):
@@ -18,25 +10,35 @@ def f_task(x):
 def approx_integral(f, base, start, end):
     matrix = []
     intercept = []
+
     for i in range(len(base)):
-        row = [gauss_legendre_formula(
-            (lambda x: x**(i + j)), 4, start, end) for j in range(len(base))]
-        intercept.append(gauss_legendre_formula(
-            (lambda x: f(x) * (x ** i)), 4, start, end))
+        row = []
+        for j in range(len(base)):
+            row.append(gauss_legendre_formula(
+                lambda x: x**(i + j), 4, start, end))
         matrix.append(row)
+        intercept.append(gauss_legendre_formula(
+            lambda x: ((x ** i) * f(x)), 4, start, end))
+
     return lu_decomposition(len(base), matrix, intercept)
 
 
-approx_integral(f_sin, base, start, end)
+start = -1
+end = 1
+base = [1, "x", "x^2", "x^3"]
+
+
+x = np_linspace(start, end, 100)
 coef = approx_integral(f_task, base, start, end)
-
-x = np_linspace(-1, 1, 100)
-pred = [horner_interpolation(coef, x_i) for x_i in x]
 real = np_array(f_task(x))
+pred = [horner_interpolation(coef[::-1], x_i) for x_i in x]
 
-plt.plot(x, pred, label='Predicted')
-plt.plot(x, real, label='Real')
+plt.title("Integral Approximation w/ Gauss-Legendre, Base: [1, x, x^2, x^3]")
+plt.plot(x, real, label="Real")
+plt.plot(x, pred, label="Predicted")
 plt.legend()
 plt.show()
 
-print(mse(pred, real))
+plt.title("Absolute Error")
+plt.plot(x, np_abs(real - pred))
+plt.show()
