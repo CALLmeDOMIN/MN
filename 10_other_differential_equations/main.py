c0e2ea0e20e904fa0c08d92b07b63f4833e36f1c
Dominik Sieroń <domin.powtorki@gmail.com>
2024-05-22 23:36:22 +0200

    Typo

--- 10_other_differential_equations/main.py
+++ 10_other_differential_equations/main.py
@@ -3,7 +3,7 @@ from numpy import cbrt as np_cbrt, arange as np_arange, log as np_log
 from numeric_methods import eulers_method
 
 
-def huens_method(f, x, y, start, end, n):
+def heuns_method(f, x, y, start, end, n):
     h = (end - start) / n
     result = []
     for _ in range(n):
@@ -64,47 +64,47 @@ def correct_fun(x):
 
 
 eulers = eulers_method(f, x, T0, start, end, n)
-huens = huens_method(f, x, T0, start, end, n)
+heuns = heuns_method(f, x, T0, start, end, n)
 midpoint = midpoint_method(f, x, T0, start, end, n)
 runge_kutta = runge_kutta_method(f, x, T0, start, end, n)
 real = [(x, correct_fun(x)) for x, _ in eulers]
 
 plot_results(real, eulers, "Euler's method")
-plot_results(real, huens, "Huen's method")
+plot_results(real, heuns, "Heun's method")
 plot_results(real, midpoint, "Midpoint method")
 plot_results(real, runge_kutta, "Runge-Kutta method")
 
 error_eulers = mse([x[1] for x in real], [x[1] for x in eulers])
-error_huens = mse([x[1] for x in real], [x[1] for x in huens])
+error_heuns = mse([x[1] for x in real], [x[1] for x in heuns])
 error_midpoint = mse([x[1] for x in real], [x[1] for x in midpoint])
 error_runge_kutta = mse([x[1] for x in real], [x[1] for x in runge_kutta])
 
 print(f"Euler's method error: {error_eulers}")
-print(f"Huen's method error: {error_huens}")
+print(f"Heun's method error: {error_heuns}")
 print(f"Midpoint method error: {error_midpoint}")
-print(f"Runge-Kutta method error: {error_runge_kutta}")
+print(f"Runge-Kutta method error: {error_runge_kutta}\n")
 
 
 eulers_errors = []
-huens_errors = []
+heuns_errors = []
 midpoint_errors = []
 runge_kutta_errors = []
 
 for i in range(10, n):
     eulers = eulers_method(f, x, T0, start, end, i)
-    huens = huens_method(f, x, T0, start, end, i)
+    heuns = heuns_method(f, x, T0, start, end, i)
     midpoint = midpoint_method(f, x, T0, start, end, i)
     runge_kutta = runge_kutta_method(f, x, T0, start, end, i)
     real = [(x, correct_fun(x)) for x, _ in eulers]
 
     eulers_errors.append(mse([x[1] for x in real], [x[1] for x in eulers]))
-    huens_errors.append(mse([x[1] for x in real], [x[1] for x in huens]))
+    heuns_errors.append(mse([x[1] for x in real], [x[1] for x in heuns]))
     midpoint_errors.append(mse([x[1] for x in real], [x[1] for x in midpoint]))
     runge_kutta_errors.append(
         mse([x[1] for x in real], [x[1] for x in runge_kutta]))
 
 plt.plot(np_arange(10, n), np_log(eulers_errors), label="Euler's method")
-plt.plot(np_arange(10, n), np_log(huens_errors), label="Huen's method")
+plt.plot(np_arange(10, n), np_log(heuns_errors), label="Heun's method")
 plt.plot(np_arange(10, n), np_log(midpoint_errors), label="Midpoint method")
 plt.plot(np_arange(10, n), np_log(
     runge_kutta_errors), label="Runge-Kutta method")
